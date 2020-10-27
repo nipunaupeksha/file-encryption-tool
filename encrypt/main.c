e461679d8f7a44c5749fbc6df47958efb2a72d36
nipunaupeksha <nipuna.upeksha@gmail.com>
2020-10-27 13:36:24 +0530

    files added

--- encrypt/main.c
+++ encrypt/main.c
@@ -16,7 +16,6 @@ int main(int argc, char *argv[]) {
     scanf("%s%s",original_name,password);
     make_new_name(&new_name,&original_name);
     printf("New filename = %s\n",new_name);
-    //printf("%s%s",filename,password);
     int password_validation = is_valid_password(&password);
 
     if(password_validation==1){
@@ -27,40 +26,18 @@ int main(int argc, char *argv[]) {
 }
 
 void print_first_five(char *filename){
-    /*
-    FILE *filePointer;
-    char ch;
-    int count=0;
-    //2
-    filePointer = fopen(filename, "rb");
-
-    if (filePointer == NULL)
-    {
-        printf("File is not available \n");
-    }
-    else
-    {
-        while ((ch = fgetc(filePointer)) != EOF)
-        {
-            printf("%x\n",(ch));
-            count++;
-            if(count>5){break;}
-        }
-    }
-    fclose(filePointer);
-    */
     FILE *fileptr;
     char *buffer;
     long filelen;
 
-    fileptr = fopen(filename, "rb");  // Open the file in binary mode
-    fseek(fileptr, 0, SEEK_END);          // Jump to the end of the file
-    filelen = ftell(fileptr);             // Get the current byte offset in the file
-    rewind(fileptr);                      // Jump back to the beginning of the file
+    fileptr = fopen(filename, "rb");
+    fseek(fileptr, 0, SEEK_END);
+    filelen = ftell(fileptr);
+    rewind(fileptr);
 
-    buffer = (char *)malloc(filelen * sizeof(char)); // Enough memory for the file
-    fread(buffer, 1, filelen+1, fileptr); // Read in the entire file
-    fclose(fileptr); // Close the file
+    buffer = (char *)malloc(filelen * sizeof(char));
+    fread(buffer, 1, filelen+1, fileptr);
+    fclose(fileptr);
     int i=0;
     for(i=0;i<5;i++){
         printf("%02x \n",buffer[i]);
@@ -72,8 +49,6 @@ void make_new_name(char *new_name, char *original_name){
 }
 
 int length_of_password(char *password){
-    //print the password to confirm that the password is passed
-    //printf("%s\n",password);
     return strlen(password);
 }
 
@@ -101,14 +76,10 @@ void perform_XOR(char *input_filename, char *output_filename, char *password){
     do {
         numBytes = fread(block, 1, blockSize, file);
         for (i=0; i<numBytes; i++){
-            //printf("%c\t%x",block[i],block[i]);
             block[i] = block[i]^password[i];
-
         }
-        // write the result of XOR to the output file
         fwrite(block, 1, numBytes, newFile);
       } while (numBytes == blockSize);
-
 
       fclose(newFile);
       fclose(file);
@@ -119,14 +90,9 @@ void perform_XOR(char *input_filename, char *output_filename, char *password){
 
 
 int is_valid_password(char *password){
-    //print the password to confirm that the password is passed
-    //printf("%s\n",password);
 
     int password_length = length_of_password(password);
     printf("Password length = %d\n",password_length);
-
-    //print the password length to confirm that the password length is correct
-    //printf("%d\n",password_length);
 
     int alphabet_count = 0;
     int digit_count = 0;
@@ -152,8 +118,6 @@ int is_valid_password(char *password){
             digit_count+=1;
         }
     }
-    //check the number of digits, letters and special character count =>special_character_count = password_length-(alphabet_count+digit_count)
-    //printf("%d %d %d\n",password_length,alphabet_count,digit_count);
 
     if(alphabet_count==0){
         printf("The password needs to contain at least one alphabetical character.\n");
